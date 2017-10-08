af7b6ba4ac864975e9579d0eaa28463076fce3a1
Kolsha <admin@ksware.ru>
2017-10-08 23:43:57 +0700

    Multiply, swap, fill, iterators and etc.

--- MultiVector/main.cpp
+++ MultiVector/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "multi_vector.h"
+
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    multi_vector<double, 3> a({1, 1, 1}), c({1, 2, 3});
-    multi_vector<double, 3> d = (a + c) * 3.33;
+    multi_vector<double, 2> a({1, 1}), c({1, 2});
+    multi_vector<double, 2> d = (a + c) * 3.33;
 
+    VECTOR_DUMP(a);
+    VECTOR_DUMP(d);
+    a.swap(d);
+    VECTOR_DUMP(a);
+    VECTOR_DUMP(d);
+
+   /* multi_vector<double, 3> m = a * c;
+
+    VECTOR_DUMP(m);
+
+    auto tmp = (a * c);
+    cout << (m == tmp) << endl;
+
+    for(auto &c:d){
+        cout << c << endl;
+    }
+    */
 
     cout << "Hello World!" << endl;
     return 0;
--- MultiVector/multi_vector.h
+++ MultiVector/multi_vector.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <iostream>
 #include <array>
-#include <initializer_list>
+
+#define VECTOR_DUMP(x) std::cout << #x << " " << x << std::endl
 
 template <typename Key_T,
           size_t dim = 3>
@@ -20,6 +21,33 @@ public:
 
     ~multi_vector(){}
 
+
+    typename std::array<Key_T, dim>::iterator begin(){
+        return coors.begin();
+    }
+    typename std::array<Key_T, dim>::const_iterator begin() const{
+        return coors.begin();
+    }
+
+    typename std::array<Key_T, dim>::iterator end(){
+        return coors.end();
+    }
+
+    typename std::array<Key_T, dim>::const_iterator end() const{
+        return coors.end();
+    }
+
+    void fill(const Key_T& val){
+        for (size_t idx = 0; idx < dim; idx++){
+            coors[idx] = val;
+        }
+    }
+    void swap(multi_vector& other){
+        for (size_t idx = 0; idx < dim; idx++){
+            std::swap(coors[idx], other[idx]);
+        }
+    }
+
     multi_vector& operator+=(const multi_vector& rhs)
     {
         for (size_t idx = 0; idx < dim; idx++){
@@ -28,18 +56,18 @@ public:
         return *this;
     }
 
-    multi_vector operator+(const multi_vector& rhs) {
-        /*multi_vector tmp;
+    multi_vector operator+(const multi_vector& rhs){
+        multi_vector tmp;
         for(size_t idx = 0; idx < dim; idx++){
             tmp[idx] = coors[idx] + rhs[idx];
         }
-        return std::move(tmp);*/
-        return (*this) += rhs;
+
+        return std::move(tmp);
     }
 
     template<typename T>
-    multi_vector& operator*=(const T& val)
-    {
+    multi_vector& operator*=(const T& val){
+
         for(size_t idx = 0; idx < dim; idx++){
             coors[idx] *= val;
         }
@@ -48,7 +76,11 @@ public:
 
     template<typename T>
     multi_vector operator*(const T& val) {
-        return (*this) *= val;
+        multi_vector tmp;
+        for(size_t idx = 0; idx < dim; idx++){
+            tmp[idx] = coors[idx] * val;
+        }
+        return std::move(tmp);
     }
 
     Key_T& operator[](size_t idx){
@@ -79,11 +111,90 @@ public:
 
 
 
-    multi_vector& operator*=(const multi_vector& rhs)
-    {
+    multi_vector& operator*=(const multi_vector& rhs){
+
+        static_assert(dim == 3, "Dimension can be only 3");
+        std::array<Key_T, dim> lhs = coors;
+
+        const size_t x = 0, y = 1, z = 2;
+
+        /*
+         * i(aybz - azby) - j(axbz - azbx) + k(axby - aybx)
+         */
+
+        coors[x] = (lhs[y] * rhs[z] - lhs[z] * rhs[y]);
+
+        coors[y] = (lhs[x] * rhs[z] - lhs[z] * rhs[x]) * (-1);
+
+        coors[z] = (lhs[x] * rhs[y] - lhs[y] * rhs[x]);
 
         return *this;
     }
 
+    multi_vector operator*(const multi_vector& rhs){
+        static_assert(dim == 3, "Dimension can be only 3");
+        std::array<Key_T, dim> lhs = coors;
+        multi_vector tmp;
+
+        const size_t x = 0, y = 1, z = 2;
+
+        /*
+         * i(aybz - azby) - j(axbz - azbx) + k(axby - aybx)
+         */
+
+        tmp[x] = (lhs[y] * rhs[z] - lhs[z] * rhs[y]);
+
+        tmp[y] = (lhs[x] * rhs[z] - lhs[z] * rhs[x]) * (-1);
+
+        tmp[z] = (lhs[x] * rhs[y] - lhs[y] * rhs[x]);
+
+        return std::move(tmp);
+    }
+
+    bool operator==(const multi_vector& rhs){
+        for (size_t idx = 0; idx < dim; idx++){
+            if(coors[idx] != rhs[idx]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const multi_vector& rhs){
+        return !((*this) == rhs);
+    }
+
+    bool operator< (const multi_vector& rhs){
+        for (size_t idx = 0; idx < dim; idx++){
+            if(coors[idx] >= rhs[idx]){
+                return false;
+            }
+        }
+        return true;
+    }
 
 };
+
+
+
+
+
+
+template <typename Key_T,
+          size_t dim = 3>
+std::ostream& operator<<(std::ostream& out, const multi_vector<Key_T, dim>& vec)
+{
+    out << "{";
+    for(size_t idx = 0; idx < dim; idx++){
+        out << vec[idx];
+        if((idx + 1) < dim){
+            out << ", ";
+        }
+    }
+    return out << "}";
+}
+
+/*
+ *
+ * http://en.cppreference.com/w/cpp/language/operators
+ */
