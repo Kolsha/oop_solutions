8805d41ee91494b2e0b910e441ee3275457b112f
Kolsha <admin@ksware.ru>
2017-10-08 19:05:45 +0700

    Multi Vector First

---
+++ MultiVector/multi_vector.cpp
@@ -0,0 +1,6 @@
+#include "multi_vector.h"
+
+multi_vector::multi_vector()
+{
+
+}
---
+++ MultiVector/multi_vector.h
@@ -0,0 +1,11 @@
+#ifndef MULTI_VECTOR_H
+#define MULTI_VECTOR_H
+
+
+class multi_vector
+{
+public:
+    multi_vector();
+};
+
+#endif // MULTI_VECTOR_H
